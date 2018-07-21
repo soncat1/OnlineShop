96335f3c841bf45598d54605e6dfbaef112aa2ab
soncat1 <siunhan1212@gmail.com>
2018-07-21 17:18:52 +0700

    add file

---
+++ OnlineShop/OnlineShop/Model/AccountModel.cs
@@ -0,0 +1,30 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Models
+{
+   public  class AccountModel
+    {
+        private OnlineShopDbContext context = null;
+        public AccountModel()
+        {
+            context = new OnlineShopDbContext();
+        }
+        public bool Login(string userName, string passWord)
+        {
+            object[] sqlParams  = new SqlParameter[]
+            {
+                new SqlParameter("@UserName",userName),
+                new SqlParameter("@PassWord",passWord),
+            };
+            var res = context.Database.SqlQuery<bool>("Sp_Account_Login @UserName,@PassWord", sqlParams).SingleOrDefault();
+            return res;
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop/OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using OnlineShop.Areas.Admin.Models;
+using Models;
+using OnlineShop.Areas.Admin.Code;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            var result = new AccountModel().Login(model.UserName, model.PassWord);
+            if(result && ModelState.IsValid)
+            {
+                SessionHelper.SetSession(new UserSession() { UserName = model.UserName });
+                return RedirectToAction("Index", "Home");
+            } 
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng.");
+            }
+            return View(model);
+        }
+    }
+}
